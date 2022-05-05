86ed3aafafb41cbf05b846c9f23ab58e9939d3a0
francocava <francocavallini@hotmail.com>
2022-05-05 18:58:58 -0300

    Starting the home

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: 'Home',
+
+  data() {
+    return {
+      drawer: false,
+    }
+  }
+})
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <hello-world />
+  <Home />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import Home from '../components/Home.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    Home,
   },
 });
 </script>
