1cc403f6491ab6bf274524019c90b4f9f8cde1aa
francocava <francocavallini@hotmail.com>
2022-06-05 19:41:15 -0300

    Upgrading the calendar

--- src/components/Calendar/calendar.vue
+++ src/components/Calendar/calendar.vue
@@ -6,6 +6,7 @@
     :disable-views="['years']"
     :events="events"
     locale="es"
+    today-button
   >
   </vue-cal>
 </template>
@@ -17,7 +18,36 @@ import 'vue-cal/dist/i18n/es.js'
 
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
 export default {
   components: { VueCal },
+
+  data: () => ({
+    events: [
+      {
+        start:'2022-6-6 10:30',
+        end:'2022-6-6 16:00',
+        title:'Cavallini - Misisco',
+        class:'leisure',
+      },
+      {
+        start:'2022-6-8 10:00',
+        end:'2022-6-10 18:00',
+        title:'Arriada - Vazquez',
+        class:'sport',
+      },
+      {
+        start:'2022-6-12 13:00',
+        end:'2022-6-12 22:00',
+        title:'Cavallini - Torasini',
+        class:'leisure',
+      },
+    ]
+  })
 }
 </script>
+
+<style>
+.vuecal__event.leisure {background-color: rgba(253, 156, 66, 0.9);border: 1px solid rgb(233, 136, 46);color: #fff;}
+.vuecal__event.sport {background-color: rgba(255, 102, 102, 0.9);border: 1px solid rgb(235, 82, 82);color: #fff;}
+</style>
