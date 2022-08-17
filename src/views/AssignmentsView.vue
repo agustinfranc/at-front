ff59ba692176b23b16a540aca2ab04a6e17fe220
francocava <francocavallini@hotmail.com>
2022-08-17 17:44:41 -0300

    feat: making the generic table with ag-grid

--- src/views/AssignmentsView.vue
+++ src/views/AssignmentsView.vue
@@ -1,18 +1,37 @@
 <template>
-  <Assignments />
+  <Assignments :columnDefinitions="columnDefs.value" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 // Components
-import Assignments from "@/components/tables/assignmentsTable.vue";
+import Assignments from "@/components/tables/genericTable.vue";
+import { reactive } from "vue"; //el onMounted va acá para la API
 
 export default defineComponent({
   name: "AssignmentsView",
 
   components: {
     Assignments,
+  },
+  setup() {
+    const rowData = reactive({}); // Set rowData to Array of Objects, one Object per Row
+
+    // Each Column Definition results in one Column.
+    const columnDefs = reactive({
+      value: [
+        { field: "Cliente" },
+        { field: "Acompañador" },
+        { field: "Fecha" },
+        { field: "Horas" },
+      ],
+    });
+
+    return {
+      columnDefs,
+      rowData,
+    };
   },
 });
 </script>
