9e7f846a65e12d18b8cd53949c7a1a809292a6e1
francocava <francocavallini@hotmail.com>
2022-08-18 17:50:14 -0300

    Revert "feat: making the generic table with ag-grid"
    This reverts commit ff59ba692176b23b16a540aca2ab04a6e17fe220.

--- src/views/AssignmentsView.vue
+++ src/views/AssignmentsView.vue
@@ -1,37 +1,18 @@
 <template>
-  <Assignments :columnDefinitions="columnDefs.value" />
+  <Assignments />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 // Components
-import Assignments from "@/components/tables/genericTable.vue";
-import { reactive } from "vue"; //el onMounted va acá para la API
+import Assignments from "@/components/tables/assignmentsTable.vue";
 
 export default defineComponent({
   name: "AssignmentsView",
 
   components: {
     Assignments,
-  },
-  setup() {
-    const rowData = reactive({}); // Set rowData to Array of Objects, one Object per Row
-
-    // Each Column Definition results in one Column.
-    const columnDefs = reactive({
-      value: [
-        { field: "Cliente" },
-        { field: "Acompañador" },
-        { field: "Fecha" },
-        { field: "Horas" },
-      ],
-    });
-
-    return {
-      columnDefs,
-      rowData,
-    };
   },
 });
 </script>
