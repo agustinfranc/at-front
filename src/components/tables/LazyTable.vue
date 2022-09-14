2b3bd8bcb98ab9ebf2549dcfc7c19f315974f468
Agustin Amato <agustin.amato96@gmail.com>
2022-09-14 16:23:33 -0300

    refactor: lazy table to composition api

--- src/components/tables/LazyTable.vue
+++ src/components/tables/LazyTable.vue
@@ -1,40 +1,34 @@
 <template>
-  <GenericTable :columns="columns" :rows="rows" />
+  <GenericTable :columns="columns" :rows="rows.value" />
 </template>
 
-<script lang="ts">
-import { defineComponent, type PropType } from "vue";
-import GenericTable from "./GenericTable.vue";
+<script setup lang="ts">
+import { reactive, onMounted, type PropType } from "vue";
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 import type { ColDef } from "@/components/tables/interfaces/GenericTable/columnDefinitions";
 
-export default defineComponent({
-  name: "LazyTable",
-  props: {
-    columns: {
-      type: Array as PropType<ColDef[]>,
-      required: true,
-    },
-    request: {
-      type: Promise,
-      required: true,
-    },
-  },
-  components: {
-    GenericTable,
-  },
+import GenericTable from "./GenericTable.vue";
 
-  data() {
-    return {
-      rows: [],
-    };
+const props = defineProps({
+  columns: {
+    type: Array as PropType<ColDef[]>,
+    required: true,
   },
-
-  async mounted() {
-    const res = await this.request();
-
-    if (!res.data) return;
-
-    this.rows = res.data;
+  request: {
+    type: Function,
+    required: true,
   },
+});
+
+// We use reactive instead of ref
+const rows = reactive({ value: [] });
+
+onMounted(async () => {
+  const res = await props.request();
+
+  if (!res.data) return;
+
+  rows.value = res.data;
 });
 </script>
