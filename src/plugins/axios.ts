adc9fba17368fb1a6e43e927241ac0f5b891ace1
Agustin <agustin.amato96@gmail.com>
2022-09-15 17:22:43 -0300

    merge to master (#23)
    * fix: mayus and unnecesary components
    
    * feat: add types for axios (#14)
    
    * feat: add types for axios
    
    * feat: axios added abstract class
    
    * refactor: add types to client table and generics (#15)
    
    * refactor: add types to client table and generics
    
    * refactor: rename interface
    
    * refactor: lazy table to composition api
    
    * refactor: client table to composition api
    
    * fix: removed styles from lazy-table
    
    * fix: build (#16)
    
    * ci: add env file (#21)

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,7 +2,8 @@ import axios, { type AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
   headers: { Authorization: "1234" },
-  baseURL: "http://localhost/api",
+  // baseURL: "http://localhost/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default instance;
