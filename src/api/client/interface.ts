1c65cfe5f0f3b58a7cdb8a827771825a8f4feb06
Agustin Amato <agustin.amato96@gmail.com>
2022-09-22 21:06:07 -0300

    doc: comments

--- src/api/client/interface.ts
+++ src/api/client/interface.ts
@@ -1,9 +1,9 @@
 export default class Client {
   id?: number;
   name: string = "";
-  dni: string = "";
-  phone: string = "";
-  rate: string = "";
-  taxable: string = "";
-  comments?: string = "";
+  dni: string = ""; // TODO: corregir tipado
+  phone: string = ""; // TODO: corregir tipado
+  rate: string = ""; // TODO: corregir tipado
+  taxable: string = ""; // TODO: corregir tipado
+  comments: string | null = "";
 }
--- src/api/companion/interface.ts
+++ src/api/companion/interface.ts
@@ -1,10 +1,10 @@
 export default class Companion {
   id?: number;
   name: string = "";
-  dni: string = "";
-  phone: string = "";
-  max_taxable: number = 0;
-  monotax?: boolean | null = null;
-  criminal_record?: boolean | null = null;
-  insurance?: boolean | null = null;
+  dni: string = ""; // TODO: corregir tipado
+  phone: string = ""; // TODO: corregir tipado
+  max_taxable: number | null = 0;
+  monotax: boolean | null = null;
+  criminal_record: boolean | null = null;
+  insurance: boolean | null = null;
 }
