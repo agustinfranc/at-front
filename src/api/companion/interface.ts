df0eae0fe5e42baafbb74ccd064429ee1744d3a9
Franco Cavallini <43447255+francocava@users.noreply.github.com>
2022-09-29 15:22:50 -0300

    upgrade client form with new fields (#38)
    * upgrading the form with the new fields
    
    * style: new empty lines
    
    * fixes to the form
    
    * fixes to the form
    
    * changing job_desc for treatment
    
    * adding sgn contract to companion & rows to client
    
    * Update src/components/forms/ClientForm.vue
    
    Co-authored-by: Agustin <agustin.amato96@gmail.com>
    
    * Update src/components/forms/ClientForm.vue
    
    Co-authored-by: Agustin <agustin.amato96@gmail.com>
    
    * correcciones
    
    Co-authored-by: Agustin Amato <agustin.amato96@gmail.com>

--- src/api/companion/interface.ts
+++ src/api/companion/interface.ts
@@ -4,9 +4,10 @@ export default class Companion {
   cuit = "";
   phone = "";
   nationality = "";
-  birth = "";
+  birthday = "";
   max_taxable = 0;
   monotax = false;
   criminal_record = false;
   insurance = false;
+  has_sign_contract = false;
 }
