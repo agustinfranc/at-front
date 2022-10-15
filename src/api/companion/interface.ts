d368f418a8299df133eb2e85f2a434787b7a677e
Agustin Amato <agustin.amato96@gmail.com>
2022-10-15 13:35:48 -0300

    fix(companioin): interface types

--- src/api/companion/interface.ts
+++ src/api/companion/interface.ts
@@ -1,11 +1,11 @@
 export default interface Companion {
   id?: number;
   name: string;
-  cuit: number;
-  phone: number;
+  cuit: string;
+  phone: string;
   nationality: string | null;
   birthday: string | null;
-  max_taxable: boolean;
+  max_taxable: number;
   monotax: boolean;
   criminal_record: boolean;
   insurance: boolean;
