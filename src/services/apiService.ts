f909d2fbba7b0a558cb76e62894fd797c655f0b4
Agustin Amato <agustin.amato96@gmail.com>
2022-10-17 13:47:40 -0300

    refactor: move method to apiService

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,5 +1,8 @@
 import type { AxiosError, AxiosPromise } from "axios";
 import { useSnackbarStore } from "@/stores/snackbar";
+import type { DeleteService } from "./interfaces/DeleteService";
+import type { FindService } from "./interfaces/FindService";
+import type { SaveService } from "./interfaces/SaveService";
 
 export interface ApiServiceResponse<T, D = ErrorApiServiceResponse> {
   data?: { data: T };
@@ -11,8 +14,34 @@ interface ErrorApiServiceResponse {
   errors?: { [key: string]: string | string[] };
 }
 
-export abstract class ApiService {
-  constructor(private snackbarStore = useSnackbarStore()) {}
+export abstract class ApiService
+  implements DeleteService, FindService, SaveService
+{
+  constructor(private _api: any, private snackbarStore = useSnackbarStore()) {}
+
+  async find<T = any>(): Promise<ApiServiceResponse<T[]>> {
+    return await this.handleRequest<T[]>(this._api.getAll);
+  }
+
+  async findOne<T>(id: number): Promise<ApiServiceResponse<T>> {
+    return await this.handleRequest<T>(this._api.getOne, id);
+  }
+
+  async create<T extends object>(item: T): Promise<ApiServiceResponse<T>> {
+    return await this.handleRequest<T>(this._api.create, {
+      ...item,
+    });
+  }
+
+  async update<T extends object>(item: T): Promise<ApiServiceResponse<T>> {
+    return await this.handleRequest<T>(this._api.update, {
+      ...item,
+    });
+  }
+
+  async delete(id: number): Promise<ApiServiceResponse<boolean>> {
+    return await this.handleRequest<boolean>(this._api.delete, id);
+  }
 
   protected async handleRequest<T>(
     request: { (param: any): AxiosPromise },
--- src/services/companionService.ts
+++ src/services/companionService.ts
@@ -9,30 +9,6 @@ export class CompanionService
   implements DeleteService, FindService
 {
   constructor(private api: CompanionApi) {
-    super();
-  }
-
-  async find(): Promise<ApiServiceResponse<Companion[]>> {
-    return await this.handleRequest<Companion[]>(this.api.getAll);
-  }
-
-  async findOne(id: number): Promise<ApiServiceResponse<Companion>> {
-    return await this.handleRequest<Companion>(this.api.getOne, id);
-  }
-
-  async create(companion: Companion): Promise<ApiServiceResponse<Companion>> {
-    return await this.handleRequest<Companion>(this.api.create, {
-      ...companion,
-    });
-  }
-
-  async update(companion: Companion): Promise<ApiServiceResponse<Companion>> {
-    return await this.handleRequest<Companion>(this.api.update, {
-      ...companion,
-    });
-  }
-
-  async delete(id: number): Promise<ApiServiceResponse<boolean>> {
-    return await this.handleRequest<boolean>(this.api.delete, id);
+    super(api);
   }
 }
