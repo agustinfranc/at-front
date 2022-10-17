34c9ff1f4d77176c0fcd7517394518a1ecb068e4
francocava <francocavallini@hotmail.com>
2022-10-17 18:50:27 -0300

    removing update & delete from service

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -10,12 +10,4 @@ export class ClientService extends ApiService {
   async _customMethodExample(): Promise<ApiServiceResponse<any>> {
     return await this.handleRequest<any>(this.api.getAll);
   }
-
-  async update(client: Client): Promise<ApiServiceResponse<Client>> {
-    return await this.handleRequest<Client>(this.api.update, { ...client });
-  }
-
-  async delete(id: number): Promise<ApiServiceResponse<boolean>> {
-    return await this.handleRequest<boolean>(this.api.delete, id);
-  }
 }
